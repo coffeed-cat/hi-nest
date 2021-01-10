3882c48a070984846235dd983b280b9388ec0456
coffeed-cat <gwcattower@gmail.com>
2021-01-10 16:37:28 +0900

    ver 1.0

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateMovieDTO } from './dto/create-movie.dto';
+import { UpdateMovieDTO } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: number): Movie {
+    console.log(typeof movieId);
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDTO) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete('/:id')
+  deleteOne(@Param('id') id: number) {
+    return this.moviesService.deleteOne(id);
+  }
+
+  @Patch('/:id')
+  update(@Param('id') id: number, @Body() updatedMovie: UpdateMovieDTO) {
+    return this.moviesService.update(id, updatedMovie);
+  }
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,78 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
     await app.init();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect("I'm App");
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+    });
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({ title: 'test', year: 1999, genres: ['test'] })
+        .expect(201);
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+
+    it('PATCH200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'forPatch', year: 2022, genres: ['action'] })
+        .expect(200);
+    });
+
+    it('PATCH400', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ id: 999 })
+        .expect(400);
+    });
+
+    it('DELETE200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
+
+    it('DELETE404', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(404);
+    });
   });
 });
